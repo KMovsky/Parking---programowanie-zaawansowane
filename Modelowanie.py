4f3e5669b9a95e908f1e53bd2d3a730c955337fc
KMovsky <9090425+KMovsky@users.noreply.github.com>
2020-06-28 09:58:05 +0200

    Add files via upload

--- Modelowanie.py
+++ Modelowanie.py
@@ -8,49 +8,28 @@ import re #regular expressions
 #dataset.csv:
 #SystemCodeNumber,Capacity,Occupancy,LastUpdated
 
-filepath = 'd:\\dev\\modelowanie\\dataset.csv'
+filepath = 'd:\\dev\\modelowanie\\dataset2.txt'
 
 datafile = open(filepath, 'r')
 lines=datafile.readlines()
 
 
-
-#with open (filepath, 'r') as datasheet:
-#    content=datasheet.read()
-#    lines=datasheet.readlines()
-count = 0
-
+Y = []
 Parking_Spots = []
 
 for line in lines:
-    #print(line.split(',')[0])
-    if line.split(',')[0] == 'BHMBCCMKT01':
-        print(line.split(',')[2])
-
 
     if line.split(',')[0] not in Parking_Spots:
-        print(str(count))
         Parking_Spots.append(line.split(',')[0])
-        print(str(line.split(',')[0]))
-        count = 1
-    else:
-        count += 1
-    #if (line.split(',')[0] != line.split(',')[0]):
-     #   print(str(count))
-    
 
-
+    if line.split(',')[0] == Parking_Spots[0]:
+        Y.append(line.split(',')[2])
 
     
-print(Parking_Spots)
 
-#regex = re.compile(r'\s\w+,')
-#mo = regex.search(content)
-#print(mo)
+print(Y)
 
 
-
-#print(content)
 
 
 datafile.close()
